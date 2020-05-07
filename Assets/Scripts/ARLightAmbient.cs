dbb1cc59cae068e5daba1f42487cf3aaed2c1db5
Thomas Day <tday42@gmail.com>
2020-05-07 11:22:39 +0100

    Added Object Placement and Light Estimation

---
+++ Assets/Scripts/ARLightAmbient.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+[RequireComponent(typeof(Light))]
+public class ARLightAmbient : MonoBehaviour
+{
+    [SerializeField]
+    private ARCameraManager m_ARCameraManager;
+
+    private Light m_Light;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Light = GetComponent<Light>();
+        m_ARCameraManager.frameReceived += OnCameraFrameReceived;
+    }
+
+    void OnCameraFrameReceived (ARCameraFrameEventArgs eventArgs)
+    {
+        m_Light.intensity = eventArgs.lightEstimation.averageBrightness.Value;
+        m_Light.colorTemperature = eventArgs.lightEstimation.averageColorTemperature.Value;
+    }
+
+    private void OnDisable()
+    {
+        m_ARCameraManager.frameReceived -= OnCameraFrameReceived;
+    }
+}
---
+++ Assets/Scripts/ObjectPlacement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class ObjectPlacement : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject m_Prefab;
+
+    private ARSessionOrigin m_SessionOrigin;
+    private ARRaycastManager m_RaycastManager;
+    private List<ARRaycastHit> m_Hits;
+    private GameObject m_BrainObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_SessionOrigin = GetComponent<ARSessionOrigin>();
+        m_RaycastManager = GetComponent<ARRaycastManager>();
+        m_Hits = new List<ARRaycastHit>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0){
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Began){
+                if (m_RaycastManager.Raycast(touch.position, m_Hits, TrackableType.PlaneWithinPolygon)){
+                    Pose pose = m_Hits[0].pose;
+                    if (m_BrainObject == null)
+                    {
+                        m_BrainObject = Instantiate(m_Prefab, pose.position, Quaternion.identity);
+                    } 
+                    else
+                    {
+                        m_BrainObject.transform.position = pose.position;
+                    }
+                }
+            }
+        }
+    }
+}
